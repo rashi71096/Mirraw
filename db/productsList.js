160ba449509eb4a283c9100d42c5225051c7e838
unknown <rashi@zaycare.com>
2020-08-21 17:06:11 +0530

    Description of products

--- db/productsList.js
+++ db/productsList.js
@@ -1,52 +1,53 @@
 const productsList = [
     {
-        id: 'P1',
-        name: 'Shirt',
-        inr: 700,
-        image: "https://bit.ly/34lhXlh",
-        description: "ABCJKKLKLHKLHKLHKLHKLHLK"
-    },
-    {
-        id: 'P2',
-        name: 'Jean',
-        inr: 520,
-        image: "https://bit.ly/32bTiNp",
-        description: "ABCJKKLKLHKLHKLHKLHKLHLK"
-    },
-    {
         id: 'P3',
         name: 'Ethnic Kurta',
         inr: 430,
         image: "https://bit.ly/34j4GK8",
-        description: "ABCJKKLKLHKLHKLHKLHKLHLK"
+        description: "Beige and pink yoke design kurta"
     },
     {
         id: 'P4',
         name: 'Jacket',
         inr: 430,
         image: "https://bit.ly/3hiFJ5l",
-        description: "ABCJKKLKLHKLHKLHKLHKLHLK"
+        description: "A comfy jacket for winters"
     },
+    {
+        id: 'P1',
+        name: 'Shirt',
+        inr: 700,
+        image: "https://bit.ly/34lhXlh",
+        description: "Linen shirt with a formal look"
+    },
+    {
+        id: 'P2',
+        name: 'Jean',
+        inr: 520,
+        image: "https://bit.ly/32bTiNp",
+        description: "Ripped jeans"
+    },
+
     {
         id: 'P5',
         name: 'T-shirt',
         inr: 430,
         image: "https://bit.ly/2CRe2BG",
-        description: "ABCJKKLKLHKLHKLHKLHKLHLK"
+        description: "Comfy all day wear T-Shirt"
     },
     {
         id: 'P6',
         name: 'Kurta',
         inr: 430,
         image: "https://bit.ly/3l6RQor",
-        description: "ABCJKKLKLHKLHKLHKLHKLHLK"
+        description: "Cotton Kurta to be wore daily"
     },
     {
         id: 'P7',
         name: 'Skirts',
         inr: 430,
         image: "https://bit.ly/3j0eGvR",
-        description: "ABCJKKLKLHKLHKLHKLHKLHLK"
+        description: "Kids Skirts"
     },
 
 ];
